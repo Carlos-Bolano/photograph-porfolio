9859bb3a570f2ba9a77429b202dd6ea8b020c4a3
carlos-bolano <carlostutos828@gmail.com>
2023-08-31 04:17:38 -0500

    upload changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+// CONST 
+// ---------------HEADER --------------
+const buttonTag = document.querySelector(".Header-button")
+const navTag = document.querySelector(".Header-nav")
+// FUNTIONS
+// ---------------HEADER --------------
+const openMenuHandler = () => {
+    navTag.classList.toggle("active")
+}
+
+// INTEGRATIONS
+// ---------------HEADER --------------
+buttonTag.addEventListener('click', () => {openMenuHandler()})
